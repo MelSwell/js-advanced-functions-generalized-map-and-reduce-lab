6a581d5ff500bb3e90da8f89c5d2dd385bf1f946
Seth M Cohen <smcohen82@gmail.com>
2021-04-15 16:23:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+const map = (arr, callback) => {
+  const newArr = []
+  for (const elem of arr) {
+    newArr.push(callback(elem))
+  }
+  return newArr
+}
+
+const reduce = (arr, callback, startingPoint) => {
+  let total = (!!startingPoint) ? startingPoint : arr[0]
+  let i = (!!startingPoint) ? 0 : 1
+  for (i = i; i < arr.length; i++) {
+    total = callback(arr[i], total)
+  }
+  return total
+}
